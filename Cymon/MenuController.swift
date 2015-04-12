59888e654b672236968693db52d6da57a1b2f0a8
Paul Zaich <pzaich@gmail.com>
2015-04-11 18:19:31 -0700

    add Asset, Models and Controllers folders

---
+++ Cymon/MenuController.swift
@@ -0,0 +1,25 @@
+//
+//  MenuController.swift
+//  Cymon
+//
+//  Created by Paul Zaich on 4/9/15.
+//  Copyright (c) 2015 Paul Zaich. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
