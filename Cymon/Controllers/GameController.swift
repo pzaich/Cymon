a85792366a6880531c169b33480c0844282b8c4e
Paul Zaich <pzaich@gmail.com>
2015-04-14 07:00:32 -0700

    Add GameController and associated view in storyboard

---
+++ Cymon/Controllers/GameController.swift
@@ -0,0 +1,29 @@
+//
+//  GameController.swift
+//  Cymon
+//
+//  Created by Paul Zaich on 4/13/15.
+//  Copyright (c) 2015 Paul Zaich. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func goBack(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+
